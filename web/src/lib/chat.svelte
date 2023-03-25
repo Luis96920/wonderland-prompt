08725cfdeb1af8490fc41aa377a164907edc54bc
luis96920 <lucasm3938@gmail.com>
2023-03-25 12:32:36 -0500

    improvements

---
+++ web/src/lib/chat.svelte
@@ -0,0 +1,9 @@
+<div class="flex flex-col justify-end h-full">
+    <div class="h-full">
+
+    </div>
+    <div class="p-4">
+        <div class="h-[2px] bg-gray-300 mb-4"/>
+        <input placeholder="Make him a robot" class="w-full rounded-lg p-2 focus:outline-none"/>
+    </div>
+</div>
--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,7 +1,9 @@
-<div class="sm:grid-cols-[3fr_1fr] grid h-[100vh]">
-<div class="bg-black" >
-</div>
-<div class="bg-gray-200"
->
-</div>
+<script>
+  import Chat from "../lib/chat.svelte";
+</script>
+
+<div class="sm:grid-cols-[7fr_3fr] grid h-[100vh]">
+  <div class="bg-black" />
+  <div class="bg-gray-200 h-full"> <Chat />
+  </div>
 </div>
