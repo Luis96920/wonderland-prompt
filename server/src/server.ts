db65615569e32984141cb1ec0c503205aacf49ca
luis96920 <lucasm3938@gmail.com>
2023-03-25 12:10:42 -0500

    initial setup of socket.io

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,27 @@
 // src/server.ts
 import * as express from "express";
+import * as socketio from "socket.io";
+import * as path from "path";
 
 const app = express();
 app.set("port", process.env.PORT || 3000);
 
 // var http = require("http").Server(app);
 import http from "node:http";
-
 let s = http.createServer(app);
-// simple '/' endpoint sending a Hello World
-// response
+
+// set up socket.io and bind it to our
+// http server.
+let io = require("socket.io")(http);
+
 app.get("/", (req: any, res: any) => {
-    res.send("hello world");
+    res.sendFile(path.resolve("./client/index.html"));
+});
+
+// whenever a user connects on port 3000 via
+// a websocket, log that a user has connected
+io.on("connection", function (socket: any) {
+    console.log("a user connected");
 });
 
 // start our simple server up on localhost:3000
