7a9a97e73a9f1bcbd3385833a3c859179b02d481
luis96920 <lucasm3938@gmail.com>
2023-03-25 12:11:23 -0500

    slightly cleaner

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,8 +1,7 @@
-<div class="sm:grid-cols-[3fr_1fr] grid h-full">
-<div class="bg-black text-3xl" >
-Test
+<div class="sm:grid-cols-[3fr_1fr] grid h-[100vh]">
+<div class="bg-black" >
 </div>
 <div class="bg-gray-200"
 >
-t2</div>
+</div>
 </div>
